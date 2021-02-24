c572d3e3e70dc4b3c963ba7c12a9b70cfea4a333
joates <77863872+joates-spring-2021@users.noreply.github.com>
2021-02-24 15:45:02 -0800

    Create Lab07Lib.py

---
+++ Lab07Lib.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+johnwoates
+CPSC 223P-01
+Thu Jan 28, 2021
+joates@fullerton.edu
+"""
+
+# Constant definition
+ASCII_LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
+ASCII_UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+DECIMAL_DIGITS = "0123456789"
+
+# Function definitions below this line
